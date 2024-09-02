76e614b7286cf334a51f7a48cf870931b247ae63
L1nk01 <ecampospaulino@gmail.com>
2024-09-02 19:12:23 -0400

    improved Router.php to recognize query parameters and removed the URL constant

--- model/Router.php
+++ model/Router.php
@@ -1,28 +1,37 @@
 <?php
 
-class Router {
+class Router
+{
   private $controller;
   private $method;
   private $config;
+  private $params = [];
 
-  public function __construct($config = null) {
+  public function __construct($config = null)
+  {
     global $database;
     $this->config = $database;
     $this->matchRoute();
   }
 
-  public function matchRoute() {
-    $url = explode('/', URL);
+  public function matchRoute()
+  {
+    $url = explode('/', trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'));
 
-    $this->controller = !empty($url[0]) ? $url[0] :'page';
+    $this->controller = !empty($url[0]) ? $url[0] : 'page';
     $this->method = !empty($url[1]) ? $url[1] : 'home';
 
     $this->controller = $this->controller . 'Controller';
 
-    require_once(dirname(__DIR__. 1) . '/controllers/' . $this->controller . '.php');
+    if (count($url) > 2) {
+      $this->params = array_slice($url, 2);
+    }
+
+    require_once(dirname(__DIR__ . 1) . '/controllers/' . $this->controller . '.php');
   }
 
-  public function run() {
+  public function run()
+  {
     $controllerName = $this->controller;
 
     $reflection = new ReflectionClass($controllerName);
@@ -35,6 +44,11 @@ class Router {
     }
 
     $method = $this->method;
-    $controller->$method();
+
+    if ($reflection->hasMethod($method)) {
+      $controller->$method();
+    } else {
+      echo "Method $method not found in $controllerName";
+    }
   }
 }
--- model/config.php
+++ model/config.php
@@ -6,9 +6,3 @@ $database = [
   'password' => '',
   'charset' => 'utf8'
 ];
-
-$folderPath = dirname($_SERVER['SCRIPT_NAME']);
-$urlPath = $_SERVER['REQUEST_URI'];
-$url = substr($urlPath, strlen($folderPath));
-
-define('URL', $url);
