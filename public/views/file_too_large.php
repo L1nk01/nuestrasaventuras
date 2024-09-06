7c9a9de1815d9441e6ab4ca7373c6685c47301e0
L1nk01 <ecampospaulino@gmail.com>
2024-09-06 06:28:46 -0400

    created view to display file too large error

---
+++ public/views/file_too_large.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Solicitud no procesada</title>
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="/public/stylesheets/fonts.css">
+  <link rel="stylesheet" href="/public/stylesheets/styles.css">
+  <link rel="stylesheet" href="/public/stylesheets/breakpoints.css">
+
+  <!-- Scripts -->
+  <script src="/public/src/index.js" defer></script>
+</head>
+
+<body>
+  <section class="error-page">
+    <img src="/public/assets/icons/sad_face.svg"/>
+    <h1>La imagen seleccionada es demasiado grande</h1>
+    <p>El tamaño máximo permitido son 5MB</p>
+    <button class="default-button" onclick="history.back()">Volver a la página anterior</button>
+  </section>
+</body>
+
+</html>
